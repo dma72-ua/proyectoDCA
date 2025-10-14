731d413a46cb3164566aac3497803667d0fa6dc3
dma72-ua <dma72@alu.ua.es>
2025-10-14 19:04:18 +0200

    Añadida funcionalidad de seguimiento de la cámara al jugador y no salirse de los bordes

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,9 @@ int main(void) {
                                    {{250, 300, 100, 10}, 1, GRAY},
                                    {{650, 300, 100, 10}, 1, GRAY}};
 
+  Camera2D camera = {
+      {screenWidth / 2.0f, screenHeight / 2.0f}, player.position, 0.0f, 1.0f};
+
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
 
@@ -32,11 +35,39 @@ int main(void) {
     Rectangle playerRect = {player.position.x - 20, player.position.y - 40,
                             40.0f, 40.0f};
     DrawRectangleRec(playerRect, BLUE);
-
     EndDrawing();
   }
 
   CloseWindow();
 
   return 0;
+}
+
+void UpdateCameraCenterInsideMap(Camera2D &camera, Player &player,
+                                 std::vector<EnvItem> &envItems, float delta) {
+  float width = GetScreenWidth();
+  float height = GetScreenHeight();
+
+  camera.target = player.position;
+  camera.offset = (Vector2){GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f};
+  float minX = 1000, minY = 1000, maxX = -1000, maxY = -1000;
+
+  for (auto &envItem : envItems) {
+    minX = std::min(envItem.rect.x, minX);
+    maxX = std::max(envItem.rect.x + envItem.rect.width, maxX);
+    minY = std::min(envItem.rect.y, minY);
+    maxY = std::max(envItem.rect.y + envItem.rect.height, maxY);
+  }
+
+  Vector2 max = GetWorldToScreen2D((Vector2){maxX, maxY}, camera);
+  Vector2 min = GetWorldToScreen2D((Vector2){minX, minY}, camera);
+
+  if (max.x < width)
+    camera.offset.x = width - (max.x - width / 2);
+  if (max.y < height)
+    camera.offset.y = height - (max.y - height / 2);
+  if (min.x > 0)
+    camera.offset.x = width / 2 - min.x;
+  if (min.y > 0)
+    camera.offset.y = height / 2 - min.y;
 }
