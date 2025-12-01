61800d552c9f5a31d88f7c695e831585e61c722a
usuario <usuario@ubuntu-20.04-desktop-amd64.jesusamieiro.com>
2025-12-01 13:49:16 +0000

    mapa centrada

--- src/levelManager.cpp
+++ src/levelManager.cpp
@@ -17,11 +17,11 @@ void LevelManager::initializeLevels() {
   // LEVEL 1 - Tutorial/Easy
   levels.push_back({
       "Nivel 1 - Primeros Pasos",
-      {60, 300},           // playerStart
+      {60, 440},           // playerStart
       {1700, 440, 8, 160}, // goal
       {
           // envItems
-          {{-4000, 400, 8000, 200}, 1, (Color){191, 111, 60, 255}, 1}, // 1 = Bricks
+          {{-4000, 440, 8000, 400}, 1, (Color){191, 111, 60, 255}, 1}, // 1 = Bricks
           {{200, 340, 160, 20}, 1, (Color){191, 111, 60, 255}, 1},
           {{500, 300, 160, 20}, 1, (Color){191, 111, 60, 255}, 1},
           {{820, 260, 120, 20}, 1, (Color){191, 111, 60, 255}, 1},
@@ -39,11 +39,11 @@ void LevelManager::initializeLevels() {
   // LEVEL 2 - Intermediate
   levels.push_back({
       "Nivel 2 - El Desafío",
-      {60, 300},           // playerStart
+      {60, 440},           // playerStart
       {2200, 440, 8, 160}, // goal
       {
           // envItems
-          {{-4000, 400, 8000, 200}, 1, (Color){191, 111, 60, 255}, 1},
+          {{-4000, 440, 8000, 400}, 1, (Color){191, 111, 60, 255}, 1},
           {{150, 340, 120, 20}, 1, (Color){191, 111, 60, 255}, 1},
           {{350, 280, 100, 20}, 1, (Color){191, 111, 60, 255}, 1},
           {{550, 220, 140, 20}, 1, (Color){191, 111, 60, 255}, 1},
@@ -68,11 +68,11 @@ void LevelManager::initializeLevels() {
   // LEVEL 3 - Advanced
   levels.push_back({
       "Nivel 3 - Maestría",
-      {60, 300},           // playerStart
+      {60, 440},           // playerStart
       {2800, 440, 8, 160}, // goal
       {
           // envItems
-          {{-4000, 400, 8000, 200}, 1, (Color){191, 111, 60, 255}, 1},
+          {{-4000, 440, 8000, 400}, 1, (Color){191, 111, 60, 255}, 1},
 
           // Long jump section
           {{200, 340, 80, 20}, 1, (Color){191, 111, 60, 255}, 1},
