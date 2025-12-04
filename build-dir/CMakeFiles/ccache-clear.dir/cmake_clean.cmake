ca18235a548e9bf4e8bd160c71e4320e34828235
dma72-ua <dma72@alu.ua.es>
2025-12-04 15:20:59 +0100

    Eliminar y dejar de trackear build-dir en github

--- build-dir/CMakeFiles/ccache-clear.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ccache-clear"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ccache-clear.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
