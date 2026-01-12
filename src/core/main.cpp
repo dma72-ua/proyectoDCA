2c8970414da9bfb61aa496a7af468c01a4457932
dma72-ua <dma72@alu.ua.es>
2026-01-12 11:56:54 +0100

    El jugador pierde la partida si cae al infinito

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -333,6 +333,12 @@ int main() {
         invincibilityTimer -= dt;
       }
 
+      // Perder la partida si se cae del mapa
+      if (player.position.y >= 800) {
+        PlaySound(audio.hurtSound);
+        state = GameState::DEFEAT;
+      }
+
       // Detectar salto para reproducir sonido
       bool wasJumping = player.vy() < 0;
       player.UpdatePlayer(currentLevel.envItems, currentLevel.teleporters, dt);
